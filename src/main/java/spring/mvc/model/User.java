c82b4a58585ac6f59d75aeeb60a11562eb22d316
Kiruha <kirillkorzunov9000@gmail.com>
2024-01-22 18:49:12 +0300

    User model

--- src/main/java/spring/mvc/model/User.java
+++ src/main/java/spring/mvc/model/User.java
@@ -1,4 +1,86 @@
 package spring.mvc.model;
 
+import javax.persistence.*;
+
+
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "surname")
+    private String surName;
+
+    @Column(name = "age")
+    private Integer age;
+
+    @Column(name = "city")
+    private String city;
+
+    public User() {
+    }
+
+    public User(String name, String surName, Integer age, String city) {
+        this.name = name;
+        this.surName = surName;
+        this.age = age;
+        this.city = city;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surName='" + surName + '\'' +
+                ", age=" + age +
+                ", city='" + city + '\'' +
+                '}';
+    }
 }
